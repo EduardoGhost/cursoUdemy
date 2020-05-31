ea5416e05043f57d41d09b91208d0f4af1e9a3c0
Eduardo <eduardo.freire@dcx.ufpb.br>
2020-05-31 20:55:27 -0300

    Endpoint /pedidos/{id} disponivel

--- src/main/java/com/example/demo/domain/ItemPedido.java
+++ src/main/java/com/example/demo/domain/ItemPedido.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class ItemPedido implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -53,11 +55,11 @@ public class ItemPedido implements Serializable{
 			return false;
 		return true;
 	}
-
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
-
+	
 	public Produto getProduto() {
 		return id.getProduto();
 	}
