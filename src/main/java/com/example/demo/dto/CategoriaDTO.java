ded117181e1ce1eb9a3ee5bf24afb910cc1ff1eb
Eduardo <eduardo.freire@dcx.ufpb.br>
2020-06-08 18:51:11 -0300

    Validacao Sintatica com Bean Validation

--- src/main/java/com/example/demo/dto/CategoriaDTO.java
+++ src/main/java/com/example/demo/dto/CategoriaDTO.java
@@ -2,14 +2,23 @@ package com.example.demo.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.example.demo.domain.Categoria;
+
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
-	private String nome;
 
+	@NotEmpty(message="Preenchimento Obrigatorio")
+	@Length(min=5, max=80, message="O temanho deve ser entre 5 e 800 caracteres")
+	
+	private String nome;
+	
 	public CategoriaDTO() {
 
 	}
