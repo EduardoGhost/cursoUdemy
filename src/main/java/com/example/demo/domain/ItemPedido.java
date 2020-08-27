d6ba99734872ed186ca09bfa2dca40562c8d3643
Eduardo <eduardo.freire@dcx.ufpb.br>
2020-08-26 21:48:46 -0300

    inserindo pedido

--- src/main/java/com/example/demo/domain/ItemPedido.java
+++ src/main/java/com/example/demo/domain/ItemPedido.java
@@ -8,9 +8,9 @@ import javax.persistence.Entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-public class ItemPedido implements Serializable{
+public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
@@ -56,18 +56,26 @@ public class ItemPedido implements Serializable{
 			return false;
 		return true;
 	}
-	
+
 	public double getSubTotal() {
 		return (preco - desconto) * quantidade;
 	}
-	
+
 	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
-	
+
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+
 	public Produto getProduto() {
 		return id.getProduto();
+	}
+
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
 	}
 
 	public ItemPedidoPK getId() {
